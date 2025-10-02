7b5c9c96ac44edba8c6f02c6064de589bffd074e
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-10-02 14:35:59 +0200

    feat(repository):  add repository for pokemon list

---
+++ src/main/java/edu/estatuas/repository/PokemonRepository.java
@@ -0,0 +1,12 @@
+package edu.estatuas.repository;
+
+import edu.estatuas.model.Pokemon;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface PokemonRepository {
+
+    void loadPokemons(String filepath) throws IOException;
+    List<Pokemon> getPokemonList() throws Exception;
+}
---
+++ src/main/java/edu/estatuas/repository/PokemonRepositoryJSON.java
@@ -0,0 +1,29 @@
+package edu.estatuas.repository;
+
+import edu.estatuas.model.Pokemon;
+import edu.estatuas.storage.PokemonJSONHandler;
+import edu.estatuas.storage.StorageHandler;
+
+import java.io.IOException;
+import java.util.List;
+
+public class PokemonRepositoryJSON implements PokemonRepository {
+
+    StorageHandler storageHandler = new PokemonJSONHandler();
+    List<Pokemon> pokemonList;
+
+    @Override
+    public void loadPokemons(String filepath) throws IOException {
+        pokemonList = storageHandler.readFromFile(filepath);
+    }
+
+    @Override
+    public List<Pokemon> getPokemonList() throws Exception{
+        if (pokemonList == null) {
+            Exception e = new Exception("No hay lista de pokemons cargada!");
+            throw e;
+        } else {
+            return pokemonList;
+        }
+    }
+}
